a7156a675559d940a08168c58eb58042f623f544
rafahuelin <rafa.huelin@deliverect.com>
2022-09-10 10:53:54 +0200

    mock requests get

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
-from main import len_joke
+from main import get_joke, len_joke
 
 
 class TestAdd(unittest.TestCase):
@@ -10,6 +10,26 @@ class TestAdd(unittest.TestCase):
     def test_len_joke(self, mock_get_joke):
         mock_get_joke.return_value = "hello"
         self.assertEqual(len_joke(), 5)
+
+    @patch("main.requests")
+    def test_get_joke(self, mock_requests):
+
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            "value": {"joke": "hello world"}
+        }
+
+        mock_requests.get.return_value = mock_response
+
+        self.assertEqual(get_joke(), "hello world")
+
+    @patch("main.requests")
+    def test_get_joke_fails(self, mock_requests):
+        mock_response = MagicMock(status_code=400)
+        mock_requests.get.return_value = mock_response
+
+        self.assertEqual(get_joke(), "No jokes")
 
 
 if __name__ == "__main__":
