b55cf75b0209985c956a5ad235789a814ee9c2ca
rafahuelin <rafa.huelin@deliverect.com>
2022-09-10 10:22:00 +0200

    add joke functions

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-def add(a, b):
-    return a + b
+import requests
+
+
+def len_joke():
+    joke = get_joke()
+    return len(joke)
+
+
+def get_joke():
+    url = "http://api.icndb.com/jokes/random"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        joke = response.json()["value"]["joke"]
+    else:
+        joke = "No jokes"
+
+    return joke
+
+
+if __name__ == "__main__":
+    print(get_joke())
