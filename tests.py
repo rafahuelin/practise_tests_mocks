8104f6344fd02191514b3f93e8c984702308759f
rafahuelin <rafa.huelin@deliverect.com>
2022-09-10 10:38:37 +0200

    mock get_joke

--- tests.py
+++ tests.py
@@ -1,11 +1,15 @@
 import unittest
+from unittest.mock import patch
 
-from main import add
+from main import len_joke
 
 
 class TestAdd(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 2), 4)
+
+    @patch("main.get_joke")
+    def test_len_joke(self, mock_get_joke):
+        mock_get_joke.return_value = "hello"
+        self.assertEqual(len_joke(), 5)
 
 
 if __name__ == "__main__":
